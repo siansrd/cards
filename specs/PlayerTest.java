65be18a68d2cec824d58229f66f3707105721688
siansrd <sianrobinsondavies@gmail.com>
2016-08-26 22:09:15 +0100

    Test players hand after deal card

--- specs/PlayerTest.java
+++ specs/PlayerTest.java
@@ -6,17 +6,26 @@ public class PlayerTest {
 
   Player player1;
   Player player2;
+  Deck deck;
+  Dealer dealer;
 
   @Before
   public void before() {
     player1 = new Player("John");
     player2 = new Player("Rudy");
+    deck = new Deck();
+    dealer = new Dealer(deck);
   }
-
 
   @Test
   public void canGetName() {
     assertEquals("John", player1.getName());
+  }
+
+  @Test
+  public void checkPlayersHand() {
+    dealer.dealCard(1, player1);
+    assertEquals(1, player1.getHand().size());
   }
 
   
