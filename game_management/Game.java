672171eb2fe6b665cd7dada3e38b2b96fc9a722d
siansrd <sianrobinsondavies@gmail.com>
2016-08-26 21:33:46 +0100

    Add a method to get player names from user

--- game_management/Game.java
+++ game_management/Game.java
@@ -1,18 +1,27 @@
 package game_management;
+import java.util.*;
 
 public class Game {
 
   public void startGame() {
     Deck deck = new Deck();
     deck.shuffle();
-    deck.printDeck();
-
+    // deck.printDeck();
+    getPlayerName();
+    
   }
 
 
   // public void alternatePlayer() {
     
   // }
+
+  public String getPlayerName() {
+    Scanner nameInput = new Scanner(System.in);
+    System.out.print("Enter Player Name: ");
+    String name = nameInput.nextLine();
+    return name;
+  }
 
 
 }
---
+++ specs/GameTest.java
@@ -0,0 +1,18 @@
+// import static org.junit.Assert.*;
+// import org.junit.*;
+// import game_management.*;
+
+// public class GameTest {
+
+//   Game game1;
+
+//   @Before
+//   public void before {
+//     game1 = new Game();
+//   }
+
+  
+
+
+// }
+
